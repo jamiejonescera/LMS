dbb7db4288defac6b11ef17612bfe94cbb6f5919
jamiejonescera <152496126+jamiejonescera@users.noreply.github.com>
2025-01-23 13:06:04 +0800

    Deploy 20

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,5 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3002,
-//     proxy: {
-//       "/api": {
-//         target: "REACT_APP_BASE_URL",
-//         secure: false,
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-
-// Load environment variables
-dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
@@ -27,8 +7,7 @@ export default defineConfig({
     port: 3002,
     proxy: {
       "/api": {
-        target: process.env.REACT_APP_BASE_URL, // Use the environment variable
-        changeOrigin: true,
+        target: "https://lms-backend-58c4.onrender.com",
         secure: false,
       },
     },
